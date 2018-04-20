1b5279ac219687df5dc0d32ecabc45888667711b
Mateusz Kłoczko <mateusz.p.kloczko@gmail.com>
2018-04-20 14:27:19 +0200

    Creating an OpenGL context.

--- src/main.cpp
+++ src/main.cpp
@@ -17,8 +17,53 @@ int main(int argc, char ** argv){
 	}
 
 	vector<Image> images = Image::loadImages(path, "image");
-	cout << "Loaded all files to the RAM." << endl;
+	//Check if all files are loaded.
+	bool not_loaded = false;
+	for(int i = 0; i < 60 && !not_loaded; i++){
+		not_loaded = images[i].data == nullptr;
+	}
 
+	if(!not_loaded){
+		cout << "All images initially loaded." << endl;
+	}
+
+	//Initialize OpenGL context.
+    if (!glfwInit()) {
+        std::cerr << "Could not initalize GLFW context." << endl;
+        return 1;
+    }
+
+
+    GLFWwindow * window = glfwCreateWindow(800, 600, "Test task", NULL, NULL);
+	//getErr("Visualiser create window");
+    if (!window) {
+        glfwTerminate();
+        std::cerr << "Error: Could not create a GLFW Window." << endl;
+        return 2;
+    }
+	glfwMakeContextCurrent(window);
+	getErr("Current context");
+
+#ifdef _WIN32
+    // Initialize GLEW
+	if (glewInit() != GLEW_OK) {
+		cerr << "Failed to initialize GLEW" << endl;
+		return 2;
+	}
+	getErr("GLEW");
+#endif
+
+    glEnable(GL_BLEND);
+	glViewport(0, 0, 800, 600);
+	while(!glfwWindowShouldClose(window)){
+
+		glClearColor(0.9,0.3,0.3,0);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glfwSwapBuffers(window);
+	}
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
 	return 0;	
 
 }
