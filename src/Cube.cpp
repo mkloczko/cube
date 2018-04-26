54a1c0ddc18f19c27ea5eed37905e5e66050cfb9
Mateusz Kłoczko <mateusz.p.kloczko@gmail.com>
2018-04-26 07:07:15 +0200

    Fixed a bug with texture sampling.

--- src/Cube.cpp
+++ src/Cube.cpp
@@ -226,6 +226,7 @@ bool Cube::initialize(){
 
     glGenTextures(1, &texture);
     glBindTexture(GL_TEXTURE_2D, texture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 1920, 1080, 0, GL_RGB, GL_UNSIGNED_BYTE, 0);
     glGenerateMipmap(GL_TEXTURE_2D);
 
